5d1b0049ae9a1ccc86d0cc4ce5013ee8d8e9a36f
Qsin <1114671386@qq.com>
2024-04-06 18:33:58 +0800

    gateway 过滤器-请求头、请求参数

--- cloud-provider-payment8001/src/main/java/com/atguigu/cloud/controller/PayGateWayController.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/cloud/controller/PayGateWayController.java
@@ -1,19 +1,25 @@
 package com.atguigu.cloud.controller;
 
+import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.util.IdUtil;
 import com.atguigu.cloud.entities.Pay;
 import com.atguigu.cloud.resp.ResultData;
 import com.atguigu.cloud.service.PayService;
 import jakarta.annotation.Resource;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Enumeration;
 
 /**
  * @Author：qinshixin
  * @Date：2024/4/4 20:47
  */
 @RestController
+@Slf4j
 public class PayGateWayController {
     @Resource
     private PayService payService;
@@ -27,5 +33,30 @@ public class PayGateWayController {
     @GetMapping(value = "/pay/gateway/info")
     public ResultData<String> getGatewayInfo() {
         return ResultData.success("gateway into test: " + IdUtil.simpleUUID());
+    }
+
+    @GetMapping(value = "/pay/gateway/filter")
+    public ResultData<String> getGatewayFilter(HttpServletRequest request) {
+        StringBuilder result = new StringBuilder();
+        Enumeration<String> headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String headName = headerNames.nextElement();
+            String headValue = request.getHeader(headName);
+            log.info("请求头名：" + headName + "\t\t\t" + "请求头值：" + headValue);
+            if ("X-Request-atguigu1".equalsIgnoreCase(headName)
+                    || "X-Request-atguigu2".equalsIgnoreCase(headName)) {
+                result.append(headName).append("\t").append(headValue).append(" ");
+            }
+        }
+
+        System.out.println("=============================================");
+        String customerId = request.getParameter("customerId");
+        System.out.println("request Parameter customerId: " + customerId);
+
+        String customerName = request.getParameter("customerName");
+        System.out.println("request Parameter customerName: " + customerName);
+        System.out.println("=============================================");
+
+        return ResultData.success("getGatewayFilter 过滤器 test：" + result + "\t" + DateUtil.now());
     }
 }
