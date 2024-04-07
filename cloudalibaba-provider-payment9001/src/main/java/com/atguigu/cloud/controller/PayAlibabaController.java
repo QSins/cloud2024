70de4ada96a27104aacf7286ff93c1d34d997a44
Qsin <1114671386@qq.com>
2024-04-07 09:29:59 +0800

    nacos服务注册之9001

---
+++ cloudalibaba-provider-payment9001/src/main/java/com/atguigu/cloud/controller/PayAlibabaController.java
@@ -0,0 +1,21 @@
+package com.atguigu.cloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author：qinshixin
+ * @Date：2024/4/7 9:18
+ */
+@RestController
+public class PayAlibabaController {
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping(value = "/pay/nacos/{id}")
+    public String getPayInfo(@PathVariable("id") Integer id) {
+        return "nacos registry, serverPort: " + serverPort + "\t id" + id;
+    }
+}
