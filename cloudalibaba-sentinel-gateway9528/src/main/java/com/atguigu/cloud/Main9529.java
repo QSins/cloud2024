8882c0a5fcf5a3a952045ea3158f9ab4c2f198e4
Qsin <1114671386@qq.com>
2024-04-09 16:14:27 +0800

    Gateway和Sentinel集成实现服务限流

---
+++ cloudalibaba-sentinel-gateway9528/src/main/java/com/atguigu/cloud/Main9529.java
@@ -0,0 +1,17 @@
+package com.atguigu.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Author：qinshixin
+ * @Date：2024/4/9 下午3:56
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class Main9529 {
+    public static void main(String[] args) {
+        SpringApplication.run(Main9529.class, args);
+    }
+}
