646cd6a39f150deb8153ffba6ac6c75877e1b78c
Qsin <1114671386@qq.com>
2024-04-09 15:29:01 +0800

    OpenFeign和Sentinel集成实现fallback服务降级

---
+++ cloud-api-commons/src/main/java/com/atguigu/cloud/apis/PayFeignSentinelApiFallBack.java
@@ -0,0 +1,18 @@
+package com.atguigu.cloud.apis;
+
+import com.atguigu.cloud.resp.ResultData;
+import com.atguigu.cloud.resp.ReturnCodeEnum;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Author：qinshixin
+ * @Date：2024/4/9 下午2:35
+ */
+@Component
+public class PayFeignSentinelApiFallBack implements PayFeignSentinelApi {
+    @Override
+    public ResultData<String> getPayByOrderNo(String orderNo) {
+        return ResultData.fail(ReturnCodeEnum.RC500.getCode(),
+                "对方服务宕机或不可用，FallBack服务降级o(╥﹏╥)o");
+    }
+}
--- cloudalibaba-consumer-nacos-order83/src/main/java/com/atguigu/cloud/controller/OrderNacosController.java
+++ cloudalibaba-consumer-nacos-order83/src/main/java/com/atguigu/cloud/controller/OrderNacosController.java
@@ -1,5 +1,7 @@
 package com.atguigu.cloud.controller;
 
+import com.atguigu.cloud.apis.PayFeignSentinelApi;
+import com.atguigu.cloud.resp.ResultData;
 import jakarta.annotation.Resource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,13 +18,20 @@ public class OrderNacosController {
     @Resource
     private RestTemplate restTemplate;
 
+    @Resource
+    private PayFeignSentinelApi payFeignSentinelApi;
+
     @Value("${service-url.nacos-user-service}")
     private String serverUrl;
 
     @GetMapping("/consumer/pay/nacos/{id}")
-    public String paymentInfo(@PathVariable("id") Integer id)
-    {
+    public String paymentInfo(@PathVariable("id") Integer id) {
         String result = restTemplate.getForObject(serverUrl + "/pay/nacos/" + id, String.class);
-        return result+"\t"+"    我是OrderNacosController83调用者。。。。。。" + System.currentTimeMillis();
+        return result + "\t" + "    我是OrderNacosController83调用者。。。。。。" + System.currentTimeMillis();
+    }
+
+    @GetMapping(value = "/consumer/pay/nacos/get/{orderNo}")
+    public ResultData<String> getPayByOrderNo(@PathVariable("orderNo") String orderNo) {
+        return payFeignSentinelApi.getPayByOrderNo(orderNo);
     }
 }
